d9c5b803561b52ecffb75c1b80eeb5784a685c35
kirby-b <kirby.brodie66@gmail.com>
2023-12-29 09:50:44 -0500

    Create helloWorld.java

---
+++ Chapter 1/1.1/helloWorld.java
@@ -0,0 +1,20 @@
+public class helloWorld {
+
+    public static void main(String[] args) {
+
+        /*
+        =============================================================
+        ========== Programming Challenge 1.1 - Hello World ==========
+        =============================================================
+
+        This is where your instructions will be for all programming challenges.
+
+        They will also contain important information about how to write your
+        software, text you will need, and some logic or math on occasion.
+
+        Please create a program that displays "Hello World" on the screen.
+
+         */
+        System.out.println("Hello World");
+    }
+}
